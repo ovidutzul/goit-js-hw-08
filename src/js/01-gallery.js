4c4cf4aeeb2e0447c6f43c66bc2098790bc3a697
ovidutzul <142990412+ovidutzul@users.noreply.github.com>
2024-01-16 13:19:24 +0200

    Add simplelighbox - ex. 1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// Descris în documentație
+import SimpleLightbox from "simplelightbox";
+// Import suplimentar de stil
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+
+const liImage = galleryItems.map( ({ original, preview, description }) =>
+ `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+ </li>`).join("");
+
+galleryContainer.insertAdjacentHTML("afterbegin", liImage);
+
+const lightbox = new SimpleLightbox(".gallery__item a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+lightbox.on("show.simpleLightbox");
+
+galleryContainer.style.listStyle = "none";

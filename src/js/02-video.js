f041af3e440385b1d855773082ab2435b340d091
ovidutzul <142990412+ovidutzul@users.noreply.github.com>
2024-01-21 18:13:10 +0200

    Homework done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+const _ = require('lodash');
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  _.throttle(function (data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+  }, 1000)
+);
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+const _ = require('lodash');
+const formFeedback = document.querySelector('.feedback-form');
 
+formFeedback.addEventListener(
+  'input',
+  _.throttle(() => {
+    const formInfo = {
+      email: formFeedback.elements.email.value,
+      message: formFeedback.elements.message.value,
+    };
+    localStorage.setItem('feedback-form-state', JSON.stringify(formInfo));
+  }, 500)
+);
+
+const parsedInfo = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+if (!parsedInfo) {
+  formFeedback.elements.email.value = '';
+  formFeedback.elements.message.value = '';
+} else if (parsedInfo) {
+  formFeedback.elements.email.value = parsedInfo.email;
+  formFeedback.elements.message.value = parsedInfo.message;
+}
+
+formFeedback.addEventListener('submit', e => {
+  const parsedInfo = JSON.parse(localStorage.getItem('feedback-form-state'));
+  e.preventDefault();
+  localStorage.removeItem('feedback-form-state');
+  formFeedback.reset();
+  console.log(parsedInfo);
+});
